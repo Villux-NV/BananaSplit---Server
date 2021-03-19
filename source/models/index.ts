54224801e4a38815c48146ea1972c197b6243575
Nick Villarreal <nick33v@gmail.com>
2021-03-19 18:21:29 -0400

    fix: small change to model index
    moved database uri into env file

--- source/models/index.ts
+++ source/models/index.ts
@@ -5,7 +5,7 @@ dotenv.config();
 
 const { DB_URI } = process.env;
 
-export = mongoose.connect('mongodb+srv://split-user:bananasplit144@bananasplit.9gbut.mongodb.net/myFirstDatabase?retryWrites=true&w=majority', {
+export = mongoose.connect(`${DB_URI}`, {
   useNewUrlParser: true,
   useUnifiedTopology: true
 })
