17f57b21aae69c9f78b77e5cb18d211f28eab518
Nick Villarreal <nick33v@gmail.com>
2021-03-19 17:49:17 -0400

    feat: added user/room models. setup server index
    Added user and room moders. Setup server index for express, router outline, and database connection

--- source/models/index.ts
+++ source/models/index.ts
@@ -1,1 +1,11 @@
-// MongoDb Atlas Connection
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const { DB_URI } = process.env;
+
+export = mongoose.connect('mongodb+srv://split-user:bananasplit144@bananasplit.9gbut.mongodb.net/myFirstDatabase?retryWrites=true&w=majority', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+})
