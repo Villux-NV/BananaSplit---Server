21215e298ee5707f701e31206726e7edcdaa4440
Nick Villarreal <nick33v@gmail.com>
2021-03-21 09:16:27 -0400

    feat: testing rooms

--- source/server.ts
+++ source/server.ts
@@ -4,18 +4,30 @@ import dotenv from 'dotenv';
 import router from './routes';
 import dbConnection from './models';
 
+import http from 'http';
+import { Server } from 'socket.io';
+
 dotenv.config();
 
-const app = express();
 const PORT = process.env.PORT || 4200;
+
+const app = express();
+const socketServer = new http.Server(app);
+
+const io = new Server(socketServer);
 
 app.use(cors());
 app.use(express.json());
 app.use('/', router);
 
+io.on('connection', (socket) => {
+  socket.emit('init', { data: 'hallo!'})
+});
+
 app.get('*', (_, res) => {
   res.status(400).send('Sorry, no page found :`(');
 });
+
 
 (async () => {
   try {
@@ -23,8 +35,13 @@ app.get('*', (_, res) => {
     console.log('Mongoose Connected');
 
     app.listen(PORT, () => {
-      console.log(`Server lives at ${PORT}`);
+      console.log(`Express Server lives at ${PORT}`);
     });
+
+    io.listen(4300)
+    // , () => {
+    //   console.log(`Socket Server lives at 4300`);
+    // });
   } catch (err) {
     console.log('Error:', err)
   }
