26b81c6fcae620e61916559593b68c7d0cb3a61f
Nick Villarreal <nick33v@gmail.com>
2021-03-23 00:05:10 -0400

    feat: changes made to handle room creation and start of join room

--- source/server.ts
+++ source/server.ts
@@ -6,6 +6,7 @@ import dbConnection from './models';
 
 import http from 'http';
 import { Server, Socket } from 'socket.io';
+import { getGameRoomCode } from './lib/utils';
 
 dotenv.config();
 
@@ -23,19 +24,60 @@ app.use(cors());
 app.use(express.json());
 app.use('/', router);
 
+// NOTE: Placing room state here, will need to extract to outside folder
+const state = {};
+const gameRooms: any = {};
+let clientCounter = 0;
+
 io.on('connection', (socket: Socket) => {
-
-  // NOTE: Send to client/single user
-  socket.emit('init', 'Hallo! Welcome to Banana/Split');
-
-  // NOTE: Send to everyone but user
+  // NOTE: Sends to client/single user
+  socket.emit('init', 'Welcome to Banana/Split');
+  // NOTE: Sends to everyone but user
   socket.broadcast.emit('init', `Player has joined`);
-
+  // NOTE: Sends to everyone
   socket.on('disconnect', () => {
     // NOTE: Send to everyone
     io.emit('init', 'Player has left');
   });
 
+  // TODO: Create game rooms and connect to front end
+  const handlePrivateGame = (code: string) => {
+    console.log('creating private game with: ', code);
+    gameRooms[socket.id] = code;
+    socket.emit('gameRoomCode', code);
+
+    socket.join(code);
+    console.log(gameRooms, 'Rooms Made');
+  };
+
+  const handleJoinGame = (code: string) => {
+    const room: any = io.sockets.adapter.rooms.get(code);
+
+    let allPlayers;
+    if (room) {
+      allPlayers = room.sockets;
+    }
+
+    let numberClients = 0;
+    if (allPlayers) {
+      numberClients = Object.keys(allPlayers).length;
+    }
+
+    if (numberClients === 0) {
+      socket.emit('unknownGame');
+      return
+    } else if (numberClients > 7) {
+      socket.emit('tooManyPlayers');
+      return
+    } 
+
+    gameRooms[socket.id] = code;
+    socket.join(code);
+
+  };
+
+  socket.on('privateGame', handlePrivateGame);
+  socket.on('joinGame', handleJoinGame);
 });
 
 app.get('*', (_, res) => {
