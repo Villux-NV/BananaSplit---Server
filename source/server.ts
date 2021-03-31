19956c9303d9eb97e48220fd1d72d8fce2055fdb
Nick Villarreal <nick33v@gmail.com>
2021-03-31 19:49:25 -0400

    stupid fix for cors link

--- source/server.ts
+++ source/server.ts
@@ -28,7 +28,7 @@ const socketServer = new http.Server(app);
 
 const io = new Server(socketServer, {
   cors: {
-    origin: 'https://banana-split-client-eio9a24sc-krista-p.vercel.app/',
+    origin: 'https://banana-split-client-eio9a24sc-krista-p.vercel.app',
     credentials: true
   }
 });
