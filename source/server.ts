6615b99f3703d67167d0650cc71c6f543b72b5ea
Nick Villarreal <nick33v@gmail.com>
2021-04-01 09:06:34 -0400

    another link

--- source/server.ts
+++ source/server.ts
@@ -28,7 +28,7 @@ const socketServer = new http.Server(app);
 
 const io = new Server(socketServer, {
   cors: {
-    origin: 'https://banana-split-client-6ku7sqev3-krista-p.vercel.app',
+    origin: 'https://banana-split-client-nb6s5anp4-krista-p.vercel.app',
     credentials: true
   }
 });
